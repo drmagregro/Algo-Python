c8293ec058d729b60e5fdc3efd6c3bba99bdf6ad
Mathis95c <114164132+Mathis95c@users.noreply.github.com>
2022-11-10 12:38:40 +0100

    Add files via upload

--- SemaineAlgo/Pseudo-Code/tri_insertion.py
+++ SemaineAlgo/Pseudo-Code/tri_insertion.py
@@ -3,19 +3,20 @@
 
 
 
-## Créer fonction tri_insertion avec argument un tableau 
+## Fonction tri_insertion avec argument un tableau
 
 ## DEBUT
 
-##     On établi la variable 'n'  = la longueur du tableau en argument
-##     On fait une boucle en créant un indice 'i' qui va de 1 jusqu'a 'n'
-##          On établi la variable cle qui stock l'indice 'i' du tableau. (la position)
-##          On établi la variable 'j' = 'i' - 1
-##          On créer une boucle Tant Que avec comme condition : 'j' strictement inférieur à 0 et cle inférieur 
+##     Créer variable 'n' = la longueur du tableau en argument
+##     Boucle avec indice 'i' qui va de 1 jusqu'a 'n'
+##          Créer variable cle = indice 'i' du tableau. (la position)
+##          Créer variable 'j' = 'i' - 1
+##          Boucle Tant Que avec comme condition : 'j' strictement inférieur à 0 et cle inférieur
 ##          à la position de l'indice 'j' dans le tableau par rapport au tour en cours
-##              On ajoute 1 à l'indice 'j' du tableau ('j'+1) 
-##              L'indice 'j' du tableau deviens le 'j'+1 du tableau
-##              On fait 'j' - 1 et on stock le resultat obtenu dans la variable 'j' déjà existante
-##          On sort du Tant Que et on ajoute 1 à l'indice 'j' du tableau et on stock cela dans la variable cle déjà existante
+##              Ajouter 1 à l'indice 'j' du tableau ('j'+1)
+##              Indice 'j' du tableau = 'j'+1 du tableau
+##              On fait 'j' - 1 = 'j'
+##          On sort du Tant Que
+##          Ajouter 1 à l'indice 'j' du tableau = cle
 
 ## FIN
--- SemaineAlgo/Pseudo-Code/tri_selection.py
+++ SemaineAlgo/Pseudo-Code/tri_selection.py
@@ -3,18 +3,18 @@
 
 
 
-## Créer fonction tri_selection avec argument un tableau 
+## Fonction tri_selection avec argument : tableau
 
 ## DEBUT
 
-##     On établi la variable n = longueur du tableau en argument
-##     On fait une boucle avec l'indice 'i' (sert de position) qui va jusqu'a n - 1 (longueur du tableau)
-##          On établi la variable pos_min (position minimale) = à l'indice 'i'
-##          On créer une boucle avec l'indice 'j' qui sert de position qui va de l'indice 'i' + 1 jusqu'a n
-##              Si la position de l'indice 'j' dans le tableau est inférieur à la position de pos_min dans le tableau 
-##                  Alors on établi la variable pos_min déjà existante = 'j'
-##          On établi la variable tmp (tempon) = la position de l'indice 'i' dans le tableau
-##          On dit que la position de l'indice 'i' dans le tableau deviens à la position de pos_min dans le tableau
-##          On dit que la position de l'indice pos_min dans le tableau = deviens la valeur présente dans la variable tmp
+##     Créer variable n = longueur du tableau en argument
+##     Boucle avec indice 'i' (sert de position) qui va jusqu'a n - 1 (longueur du tableau)
+##          Créer variable pos_min (position minimale) = à l'indice 'i'
+##          Boucle avec indice 'j' (sert de position) qui va de l'indice 'i' + 1 jusqu'a n
+##              Si position de l'indice 'j' dans le tableau inférieur à la position de pos_min dans le tableau
+##                  Alors variable pos_min = 'j'
+##          Créer variable tmp (tempon) = la position de l'indice 'i' dans le tableau
+##          Position de l'indice 'i' dans le tableau = position de pos_min dans le tableau
+##          Position de l'indice pos_min dans le tableau = tmp
 
 ## FIN
